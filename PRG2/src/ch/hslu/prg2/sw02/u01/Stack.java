94239cfe401cfdcae6075daa12ccc7e873800267
frowin <frowin@frowin-notebook>
2015-02-27 11:49:56 +0100

    gem. Folie 18 (DAT2)

--- PRG2/src/ch/hslu/prg2/sw02/u01/Stack.java
+++ PRG2/src/ch/hslu/prg2/sw02/u01/Stack.java
@@ -10,5 +10,33 @@ package ch.hslu.prg2.sw02.u01;
  * @author frowin
  */
 public class Stack {
-    
+
+    private int size;
+    private int top = 0;
+
+    private Object[] stack;
+
+    public Stack(int s) {
+        size = s;
+        stack = new Object[size];
+    }
+
+    public void push(Object o) {
+        stack[top] = o;
+        top++;
+    }
+
+    public Object pop() {
+        top--;
+        return stack[top];
+    }
+
+    public boolean isEmpty() {
+        return (top == 0);
+    }
+
+    public boolean isFull() {
+        return (top == size);
+    }
+
 }
