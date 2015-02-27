2a1e2db9fcd65178e4068d98ed01b7b8f9ef0835
frowin <frowin@frowin-notebook>
2015-02-27 18:09:02 +0100

    gem. Folie 18 (DAT2)

--- PRG2/src/ch/hslu/prg2/sw01/u02/ListNode.java
+++ PRG2/src/ch/hslu/prg2/sw01/u02/ListNode.java
@@ -5,10 +5,36 @@
  */
 package ch.hslu.prg2.sw01.u02;
 
+import ch.hslu.prg2.sw01.u01.Konto;
+
 /**
  *
- * @author tobias
+ * @author frowin
  */
-public class ListNode {
-    
+public class ListNode<T> {
+
+    private T data;
+    private ListNode<T> next;
+
+    public ListNode(ListNode<T> n, T d) {
+        this.next = n;
+        this.data = d;
+    }
+
+    public void setData(T d) {
+        this.data = d;
+    }
+
+    public T getData() {
+        return this.data;
+    }
+
+    public void setNext(ListNode<T> n) {
+        this.next = n;
+    }
+
+    public ListNode<T> getNext() {
+        return this.next;
+    }
+
 }
